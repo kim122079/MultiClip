0fed6db034725ac546f71554b456e564d08a6afd
otw <otw@otw-desktop.(none)>
2010-01-31 01:02:50 +0100

    implemented popup on mouseup when text is selected, if popup is clicked text is clipped, if user clicks outside dialog dialog is dismissed

--- 0.1/content.js
+++ 0.1/content.js
@@ -1,12 +1,14 @@
 const port = chrome.extension.connect({name: "MultiClip"});
+var pageX;
+var pageY;
+var clipDialog = true;
 
 port.onMessage.addListener( function(msg) {
   if (msg.status)
     	showStatusMsg(msg.status);
 });
 
-function postSelectedText() {
-	var text = window.getSelection().toString();
+function postSelectedText(text) {
 	if (text != '') {
 		port.postMessage({clip: text});
 	} 
@@ -15,6 +17,45 @@ function postSelectedText() {
 $().keypress(function(event) {
 	//3 == Strg+Shift+C
     	if (event.which == 3) {
-		postSelectedText();		
+		postSelectedText(window.getSelection().toString());		
     	}
 });
+
+$().mousemove(function(event){
+	pageX = event.pageX;
+	pageY = event.pageY;
+});
+
+
+$().mouseup(function(){
+	if(clipDialog)	
+		showDialog(pageX, pageY);
+});
+
+function showDialog(xPos, yPos) {
+	var selection = window.getSelection().toString();
+	var active;
+	if(selection != null && selection != "") {
+		active = true;		
+		$('body').append('<div id="dialog">clip</div>');
+		$("#dialog").css("left", xPos-2).css("top", yPos-46);
+		$("#dialog:hidden").toggle("fast");	
+		$().mousedown(function(){
+			if(active) {
+				$("#dialog").toggle("fast",function(){
+	   	   			$("#dialog").remove();
+					active = false;
+    				});	
+			}
+		});
+
+		$("#dialog").mousedown(function(){
+			postSelectedText(selection);
+		});
+		setTimeout(function() {
+			$("#dialog").toggle("fast",function(){
+   	   			$("#dialog").remove();
+    			});
+		},3000);
+	}
+}
